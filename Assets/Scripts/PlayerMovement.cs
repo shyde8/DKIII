9e4e60909a36ad9f333f2816645ce2959b1df1cd
shyde8 <shyde8@gmail.com>
2018-10-21 19:37:35 -0400

    finished ladder upward and downward movement
    finished ladder upward and downward movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -24,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
     private float _defGravityScale;
     private bool _isGrounded = true;
     private bool _isClimbing = false;
-       
+
 
     //public variables
     public float speed = 150.0f;
@@ -37,7 +37,7 @@ public class PlayerMovement : MonoBehaviour
     public const float PLATFORM_THICKNESS = 0.4f;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         //get gameobject components
         _body = GetComponent<Rigidbody2D>();
@@ -51,16 +51,16 @@ public class PlayerMovement : MonoBehaviour
 
         //grab default gravity scale
         _defGravityScale = _body.gravityScale;
-	}
+    }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
         #region Horizontal Movement
         //only apply movement if either left or right arrow are down, to avoid "floaty" behavior
         float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         Vector2 movement = new Vector2(deltaX, _body.velocity.y);
-		if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) && _isGrounded && !_isClimbing)
+        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) && _isGrounded && !_isClimbing)
         {
             _body.velocity = movement;
             _anim.SetBool("isHorKeyDown", true);
@@ -83,10 +83,10 @@ public class PlayerMovement : MonoBehaviour
         _isGrounded = false;
 
         //player should only be "grounded" if the object below them is marked as ground layer (note: currently we're using a hard-coded value of 9 to check equality)
-        if (hit != null && hit.GetComponent<Collider2D>().gameObject.layer==9)
+        if (hit != null && hit.GetComponent<Collider2D>().gameObject.layer == 9)
         {
             _isGrounded = true;
-        }        
+        }
 
         //jumping
         if (_isGrounded && Input.GetKeyDown(KeyCode.Space) && !_isClimbing)
@@ -115,24 +115,37 @@ public class PlayerMovement : MonoBehaviour
         #endregion
 
         #region Ladder Movement
-        RaycastHit2D ladderHit = Physics2D.Raycast(transform.position, Vector2.up, ladderDetectionDistance, ladder);        
-        if (ladderHit.collider != null)
+        RaycastHit2D ladderUp = Physics2D.Raycast(transform.position, Vector2.up, ladderDetectionDistance, ladder);
+        RaycastHit2D ladderDown = Physics2D.Raycast(transform.position, Vector2.down, ladderDetectionDistance + 1f, ladder);
+        if (ladderUp.collider != null && Input.GetKey(KeyCode.UpArrow))
         {
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                _isClimbing = true;
-                _body.gravityScale = 0; //gravity should not apply to jumpman while climbing                
-                //set x position to that of the ladder, to "lock" the player into it
-                Vector2 newPos = new Vector2(ladderHit.transform.position.x, transform.position.y);
-                _body.MovePosition(newPos);
-                //apply initial small burst upward if we're grounded, so we're no longer grounded after initially entering climbing mode
-                if (_isGrounded)
-                    _body.AddForce(Vector2.up * climbSpeed, ForceMode2D.Impulse);
-            }                               
+            _isClimbing = true;
+            _body.gravityScale = 0; //gravity should not apply to jumpman while climbing                
+                                    //set x position to that of the ladder, to "lock" the player into it
+            Vector2 newPos = new Vector2(ladderUp.transform.position.x, transform.position.y);
+            _body.MovePosition(newPos);
+            //apply initial small burst upward if we're grounded, so we're no longer grounded after initially entering climbing mode
+            if (_isGrounded)
+                _body.AddForce(Vector2.up * climbSpeed, ForceMode2D.Impulse);
+        }
+        else if (ladderDown.collider != null && Input.GetKey(KeyCode.DownArrow))
+        {          
+            _isClimbing = true;
+            _body.gravityScale = 0; //gravity should not apply to jumpman while climbing                
+                                    //set x position to that of the ladder, to "lock" the player into it
+            Vector2 newPos = new Vector2(ladderDown.transform.position.x, transform.position.y);
+            _body.MovePosition(newPos);
+
+            //only set isTrigger to true if there's no ladder immediately above you
+            if (hit != null && ladderUp.collider == null)
+                hit.gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+            //apply initial small burst downward if we're grounded, so we're no longer grounded after initially entering climbing mode
+            if (_isGrounded)
+                _body.AddForce(Vector2.down * climbSpeed, ForceMode2D.Impulse);
         }
 
         if (_isClimbing)
-        {            
+        {
             _body.velocity = Vector3.zero; //set velocity to zero in each frame in climbing-mode to eliminate momentum
             if (Input.GetKey(KeyCode.UpArrow))
             {
@@ -145,19 +158,26 @@ public class PlayerMovement : MonoBehaviour
             //exit climbing mode if touching the ground, and if a downward raycast from bottom of jumpman offset by platform thickness is not touching a ladder
             Vector2 startPos = new Vector2(transform.position.x, _box.bounds.min.y);
             Vector2 endPos = transform.TransformDirection(Vector2.down);
-            RaycastHit2D platformHit = Physics2D.Raycast(startPos, Vector2.down, PLATFORM_THICKNESS, ladder);            
-            if (_isGrounded && platformHit.collider==null)
+            RaycastHit2D platformHit = Physics2D.Raycast(startPos, Vector2.down, PLATFORM_THICKNESS, ladder);
+            if (_isGrounded && platformHit.collider == null)
             {
-
                 _isClimbing = false;
             }
-
         }
+
         else if (!_isClimbing)
         {
             _body.gravityScale = _defGravityScale;
         }
+
         #endregion
 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //set isTrigger for hit == false, only for ground
+        if (collision.GetComponent<Collider2D>().gameObject.layer == 9 && collision.gameObject.GetComponent<BoxCollider2D>().isTrigger == true)
+            collision.gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
     }
 }
