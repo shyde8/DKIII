0a95ef5f1f88ba11e89f7eb94ccbe78ab54c2224
shyde8 <32993251+shyde8@users.noreply.github.com>
2018-10-27 21:10:05 -0400

    initial scripts for touching enemies
    initial scripts for touching enemies

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCharacter : MonoBehaviour
+{
+
+    //for now, we just reset the scene when player touches a ReactiveEnemy
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        ReactiveEnemy enemy = col.gameObject.GetComponent<ReactiveEnemy>();
+        if (enemy != null)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+
+}
