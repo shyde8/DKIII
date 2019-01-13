c6815e8af739547a39ef3db1635a90be73ceb491
shyde8 <shyde8@gmail.com>
2019-01-12 21:12:39 -0500

    detect jumping over enemies
    detect jumping over enemies

---
+++ Assets/Scripts/EnemyJumpDetector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyJumpDetector : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip scoreSound;
+    private AudioSource _audio;
+    private BoxCollider2D _box;
+    private float detectionDistance = .8f;
+    private int framesBetweenScores = 0;
+    private int framesBetweenScoresThreshold = 15;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _box = GetComponent<BoxCollider2D>();
+        _audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        framesBetweenScores++;
+        Vector2 startPos = new Vector2(transform.position.x, _box.bounds.min.y);
+        RaycastHit2D enemyHit = Physics2D.Raycast(startPos, Vector2.down, detectionDistance, 1 << LayerMask.NameToLayer("Enemy"));
+        if(enemyHit.collider != null)
+        {
+            if (enemyHit.collider.gameObject.GetComponent<ReactiveEnemy>() != null)
+            {
+                if(framesBetweenScores >= framesBetweenScoresThreshold)
+                {
+                    Debug.Log("Here");
+                    _audio.PlayOneShot(scoreSound);
+                    framesBetweenScores = 0;
+                }                
+            }
+        }        
+	}
+}
---
+++ Assets/Scripts/TaxiFallStick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaxiFallStick : MonoBehaviour
+{
+    private Rigidbody2D _body;
+	// Use this for initialization
+	void Start ()
+    {
+        _body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<ConveyorBelt>() != null)
+        {
+            Debug.Log("Here");
+            StartCoroutine(PauseForSecond());
+        }
+    }
+
+    private IEnumerator PauseForSecond()
+    {
+        _body.constraints = RigidbodyConstraints2D.FreezePositionX;
+        yield return new WaitForSeconds(0.2f);
+        
+        _body.constraints = RigidbodyConstraints2D.None;
+    }
+}
